224c0163e7c1ad9a40087e0083096689e7162992
viniciusgois711 <89846233+viniciusgois711@users.noreply.github.com>
2022-10-17 18:18:02 -0300

    Add files via upload

---
+++ dominio/PortaSala.java
@@ -0,0 +1,5 @@
+package dominio;
+
+public class PortaSala extends Porta{
+	
+}
---
+++ visao/Principal.java
@@ -0,0 +1,10 @@
+package visao;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
